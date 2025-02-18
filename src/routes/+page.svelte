04898ea10db2e18c19de180c4f9e2b35e60af6ae
Ja-boop <agusmisiones@gmail.com>
2025-02-18 17:21:33 -0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let isConnected = $state(false);
+
+	function onClick() {
+		isConnected = !isConnected;
+	}
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<button onclick={onClick}>{isConnected ? 'Disconnect' : 'Connect'}</button>
