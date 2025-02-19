fde79e072a39e7c47769fcf0199514b559f7a5f0
Ja-boop <agusmisiones@gmail.com>
2025-02-19 14:56:43 -0300

    Add Wagmi and MetaMask implementation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 	import '../app.css';
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
+
+	const queryClient = new QueryClient();
 	let { children } = $props();
 </script>
 
-{@render children()}
+<main>
+	<QueryClientProvider client={queryClient}>
+		{@render children()}
+	</QueryClientProvider>
+</main>
