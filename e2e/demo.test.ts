da20610cdb1e909160d3c27f8695e65e8ef69d9b
Viktor Agustin Luzny <56199168+Ja-boop@users.noreply.github.com>
2025-02-20 19:45:37 -0300

    Add playwright test (#5)
    Co-authored-by: Ja-boop <agusmisiones@gmail.com>

--- e2e/demo.test.ts
+++ e2e/demo.test.ts
@@ -1,6 +1,16 @@
 import { expect, test } from '@playwright/test';
 
-test('home page has expected h1', async ({ page }) => {
+test('has title', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.locator('h1')).toBeVisible();
+	await expect(page).toHaveTitle(/USDT Balance - Sepolia/);
+});
+
+test('has info text', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByText('Check your USDT balance for Sepolia network')).toBeVisible();
+});
+
+test('connect wallet button is visible', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByText('Connect your wallet')).toBeVisible();
 });
