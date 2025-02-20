180519f56ef52c2b1d3ee3bf6969bcf252f572eb
Ja-boop <agusmisiones@gmail.com>
2025-02-20 15:09:03 -0300

    Add wallet composable

--- src/lib/components/Dropdown.svelte
+++ src/lib/components/Dropdown.svelte
@@ -3,42 +3,35 @@
 	import { slide } from 'svelte/transition';
 	import CheckIcon from '$lib/icons/CheckIcon.svelte';
 	import { WalletName } from '$lib/types';
-	import { useSelectedWallet } from '$lib/composables/wallet.svelte';
-	import getWallet from '$lib/wallets/WalletFactory';
+	import { wallet, setWallet } from '$lib/composables/wallet.svelte';
 
 	const wallets: WalletName[] = Object.values(WalletName);
 	let isOpen = $state(false);
 
 	function handleSelectWallet(wallet: WalletName) {
-		useSelectedWallet.selectedWallet = getWallet(wallet);
+		setWallet(wallet);
 	}
 </script>
 
 <div class="relative w-fit">
 	<button class="flex cursor-pointer flex-row" onclick={() => (isOpen = !isOpen)}>
-		{useSelectedWallet.selectedWallet.name}
-		<img
-			alt={useSelectedWallet.selectedWallet.name}
-			src={useSelectedWallet.selectedWallet.icon}
-			width="24px"
-		/>
+		{wallet.selected.name}
+		<img alt={wallet.selected.name} src={wallet.selected.icon} width="24px" />
 		<ArrowIcon direction={isOpen ? 'up' : 'down'} />
 	</button>
 
 	{#if isOpen}
 		<ul transition:slide class="absolute flex w-auto flex-col">
-			{#each wallets as wallet}
+			{#each wallets as name}
 				<li
-					class={`${useSelectedWallet.selectedWallet.name === wallet ? 'bg-primary-500/30' : 'bg-primary-500/10'} 
+					class={`${wallet.selected.name === name ? 'bg-primary-500/30' : 'bg-primary-500/10'} 
                     flex cursor-pointer flex-row items-center border-b border-black/20 px-5 py-1 first:rounded-t-sm last:rounded-b-sm last:border-b-0`}
 				>
-					<button class="cursor-pointer" onclick={() => handleSelectWallet(wallet)}>
-						{wallet}
+					<button class="cursor-pointer" onclick={() => handleSelectWallet(name)}>
+						{name}
 					</button>
 
-					<CheckIcon
-						class={`${useSelectedWallet.selectedWallet.name === wallet ? 'opacity-100' : 'opacity-0'}`}
-					/>
+					<CheckIcon class={`${wallet.selected.name === name ? 'opacity-100' : 'opacity-0'}`} />
 				</li>
 			{/each}
 		</ul>
