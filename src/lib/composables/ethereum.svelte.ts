964f3ffb2c4f31470f5b3e1a0c88782543700aa6
Viktor Agustin Luzny <56199168+Ja-boop@users.noreply.github.com>
2025-02-20 00:05:18 -0300

    [FEATURE] Home page (#2)

--- src/lib/composables/ethereum.svelte.ts
+++ src/lib/composables/ethereum.svelte.ts
@@ -1,4 +1,4 @@
-import { createConfig, http, getAccount, connect, disconnect } from '@wagmi/core';
+import { createConfig, http, getAccount, connect, disconnect, watchAccount } from '@wagmi/core';
 import { sepolia } from '@wagmi/core/chains';
 import { metaMask } from '@wagmi/connectors';
 import { getBalanceQueryOptions } from '@wagmi/core/query';
@@ -14,36 +14,42 @@ const config = createConfig({
 });
 
 function useEthereum() {
-	let address = $state(getAccount(config).address);
+	let account = $state(getAccount(config));
+
+	const unwatch = watchAccount(config, {
+		onChange(data) {
+			account = data;
+		}
+	});
 
 	async function useConnect() {
 		await connect(config, { connector: metaMask() });
-		address = getAccount(config).address;
+		account = getAccount(config);
 	}
 
 	async function useDisconnect() {
-		await disconnect(config, { connector: getAccount(config).connector });
-		address = undefined;
+		await disconnect(config, { connector: account.connector });
 	}
 
 	function useBalance() {
 		return createQuery({
 			...getBalanceQueryOptions(config, {
-				address,
+				address: account.address,
 				token: PUBLIC_USDT_CONTRACT_ADDRESS as `0x${string}`
 			}),
-			enabled: Boolean(address),
+			enabled: account.isConnected,
 			retry: false
 		});
 	}
 
 	return {
-		get address() {
-			return address;
+		get account() {
+			return account;
 		},
 		useConnect,
 		useDisconnect,
-		useBalance
+		useBalance,
+		unwatch
 	};
 }
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,102 @@
 <script lang="ts">
+	import { slide } from 'svelte/transition';
 	import useEthereum from '$lib/composables/ethereum.svelte';
+	import RefreshIcon from '$lib/icons/RefreshIcon.svelte';
+	import { truncateText } from '$lib/utils';
+	import { onDestroy } from 'svelte';
+	import Button from '$lib/components/Button.svelte';
+	import CloseIcon from '$lib/icons/CloseIcon.svelte';
+	import illustration from '$lib/assets/illustration.png';
 
-	const { useConnect, useDisconnect, address, useBalance } = $derived(useEthereum());
+	const { useConnect, useDisconnect, account, useBalance, unwatch } = $derived(useEthereum());
 	const balance = $derived(useBalance());
-
-	async function onClick() {
-		if (address) {
-			await useDisconnect();
-		} else {
-			await useConnect();
+	const isLoading = $derived(account.address && $balance.isPending);
+	const balanceText = $derived.by(() => {
+		if (isLoading) {
+			return 'Loading...';
 		}
-	}
+
+		if ($balance.error) {
+			return 'There was a problem, please try again';
+		}
+
+		if ($balance.isSuccess) {
+			return `${$balance.data.formatted} USDT`;
+		}
+	});
+
+	onDestroy(() => {
+		unwatch();
+	});
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+	<title>USDT Balance - Sepolia</title>
+</svelte:head>
 
-<p>Address: {address}</p>
+<div
+	class="relative mx-6 mt-40 flex flex-col items-center justify-center gap-24 lg:flex-row lg:gap-72"
+>
+	<div class="flex flex-col gap-3 self-start">
+		<h1 class="text-3xl font-semibold lg:text-5xl">Check your balance</h1>
+		<h2 class="text-xl font-normal lg:w-70 lg:text-3xl/11">
+			Check your USDT balance for Sepolia network
+		</h2>
+	</div>
 
-<button onclick={onClick}>{address ? 'Disconnect' : 'Connect'}</button>
+	<img
+		class="absolute top-10 hidden lg:block"
+		alt="illustration"
+		src={illustration}
+		width="300px"
+	/>
 
-{#if address}
-	<div>
-		{#if $balance.isPending}
-			Loading...
-		{/if}
-		{#if $balance.error}
-			An error has occurred:
-			{$balance.error.message}
-		{/if}
-		{#if $balance.isSuccess}
-			<div>
-				<strong>👀 {$balance.data.formatted}</strong>{' '}
+	<div class="flex w-full flex-col gap-7 lg:h-80 lg:w-96">
+		{#if account.address}
+			<h3 transition:slide class="text-xl font-medium">Balance:</h3>
+
+			<div
+				transition:slide
+				class="bg-primary-500/10 relative flex h-40 flex-col items-center justify-center rounded-lg px-5 py-14 text-center"
+			>
+				<p
+					class={`${$balance.isError ? 'text-xl' : 'text-4xl'} font-semibold ${isLoading ? 'animate-pulse' : ''}`}
+				>
+					{balanceText}
+				</p>
+
+				{#if $balance.error}
+					<button
+						transition:slide
+						onclick={() => $balance.refetch()}
+						class="absolute bottom-0 left-0 m-2 cursor-pointer"
+						disabled={isLoading}
+					>
+						<RefreshIcon />
+					</button>
+				{/if}
 			</div>
 		{/if}
+
+		<div class="relative w-full text-center">
+			{#if account.address}
+				<p class="bg-primary-500 rounded-lg py-4 text-white">
+					{truncateText(account.address, 8)}
+				</p>
+			{:else}
+				<Button onclick={useConnect} disabled={account.isConnected}>
+					<p class="py-4 text-white">Connect your wallet</p>
+				</Button>
+			{/if}
+
+			{#if account.address}
+				<button
+					onclick={useDisconnect}
+					class="absolute top-1/2 right-0 -translate-x-1/2 -translate-y-1/2 cursor-pointer"
+				>
+					<CloseIcon />
+				</button>
+			{/if}
+		</div>
 	</div>
-{/if}
+</div>
