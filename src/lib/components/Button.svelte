2e3514df128ba4a06beb9212e4f36e8408c352c6
Ja-boop <agusmisiones@gmail.com>
2025-02-19 21:28:43 -0300

    Add Button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { cva } from 'class-variance-authority';
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+
+	interface Props extends Omit<HTMLButtonAttributes, 'class'> {
+		color?: 'primary';
+		children: Snippet;
+	}
+
+	let { color = 'primary', children, ...rest }: Props = $props();
+
+	const buttonClasses = cva([`w-full flex flex-col items-center justify-center rounded-lg`], {
+		variants: {
+			color: {
+				primary: 'bg-primary-500'
+			}
+		}
+	});
+</script>
+
+<button class={buttonClasses({ color })} {...rest}>
+	{@render children()}
+</button>
